138c4eb24230dc2b7cdf3a3ff2aa40e30c711c42
Muath Khalifa <muath.khalifa@yahoo.com>
2024-03-22 02:12:35 +0000

    ability to upload photos

--- convex/sketches.ts
+++ convex/sketches.ts
@@ -1,3 +1,4 @@
+import { internalActionGeneric } from "convex/server";
 import { internal } from "./_generated/api";
 import {
   internalAction,
@@ -7,11 +8,23 @@ import {
 } from "./_generated/server";
 import { v } from "convex/values";
 
+import Replicate from "replicate";
+const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN });
+
 export const saveSketch = mutation({
   args: { prompt: v.string(), image: v.string() },
-  handler: async (ctx, { prompt }) => {
+  handler: async (ctx, args) => {
+    const { prompt, image } = args;
     const sketch = await ctx.db.insert("sketches", {
       prompt,
+    });
+
+    console.log("sketch: ", sketch);
+
+    await ctx.scheduler.runAfter(0, internal.sketches.generate, {
+      sketchId: sketch,
+      prompt,
+      image,
     });
 
     return {
@@ -21,10 +34,39 @@ export const saveSketch = mutation({
 });
 
 export const generate = internalAction(
-  ({}, { prompt, image }: { prompt: string; image: string }) => {
+  async (
+    { runMutation },
+    {
+      prompt,
+      image,
+      sketchId,
+    }: { sketchId: any; prompt: string; image: string }
+  ) => {
+    console.log("running replicate..............");
+    const input = {
+      image,
+      scale: 7,
+      prompt,
+      image_resolution: "512",
+      n_prompt:
+        "longbody, lowers, bad anatomy, bad hand, missing finger, extra digit",
+    };
+
+    const output = (await replicate.run(
+      "tencentarc/gfpgan:0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c",
+      { input }
+    )) as [string, string];
+    console.log(output);
+
+    runMutation(internal.sketches.updateSketchResult, {
+      sketchId,
+      result: output[1],
+    });
+    //=> "https://replicate.deliv
     //implement
   }
 );
+
 // export const getSketch = query({
 //   args: { sketchId: v.id("sketches") },
 //   handler: (ctx, { sketchId }) => {
@@ -33,14 +75,14 @@ export const generate = internalAction(
 //   },
 // });
 
-// export const updateSketchResult = internalMutation({
-//   args: { sketchId: v.id("sketches"), result: v.string() },
-//   handler: async (ctx, { sketchId, result }) => {
-//     await ctx.db.patch(sketchId, {
-//       result,
-//     });
-//   },
-// });
+export const updateSketchResult = internalMutation({
+  args: { sketchId: v.id("sketches"), result: v.string() },
+  handler: async (ctx, { sketchId, result }) => {
+    await ctx.db.patch(sketchId, {
+      result,
+    });
+  },
+});
 
 export const getSketches = query({
   handler: async (ctx) => {
